c4d89036f92d58dd0cb618bd8a91c706aaf21687
Nicholas Miller <nicholas.miller@ufl.edu>
2017-11-30 19:34:48 -0500

    added nodeInFrontOf added isPakuOnPath

--- src/ufl/cs1/controllers/StudentController.java
+++ src/ufl/cs1/controllers/StudentController.java
@@ -16,8 +16,8 @@ import java.util.List;
         public int[] update(Game game, long timeDue) {
             int[] actions = new int[Game.NUM_DEFENDER];
 
-                actions[0] = superTrapperUpdate(game.getAttacker(), game.getDefender(0), game, game.getCurMaze());
-                actions[1] = trapperUpdate(game.getAttacker(), game.getDefender(1), game.getCurMaze(),game);
+                actions[0] = trapperUpdate(game.getAttacker(), game.getDefender(0), game, game.getCurMaze());
+                actions[1] = baitUpdate(game.getAttacker(), game.getDefender(1), game.getCurMaze(),game);
                 actions[2] = pincherUpdate(game.getAttacker(), game.getDefender(2), game.getCurMaze(),game);
                 actions[3] = dumbChaserUpdate(game.getAttacker(), game.getDefender(3), game.getCurMaze(),game);
 
@@ -26,7 +26,7 @@ import java.util.List;
 
         // ** Ghost AI methods **
 
-        private int trapperUpdate(Attacker attacker, Defender defender,Maze m,Game game) {
+        private int baitUpdate(Attacker attacker, Defender defender,Maze m,Game game) {
             if(PakuOnPowerPill(attacker,m)){
                 return run(defender,m,attacker,game,3);
             }
@@ -55,24 +55,41 @@ import java.util.List;
                 return defender.getNextDir(target, !defender.isVulnerable());
         }
 
-        private int superTrapperUpdate(Attacker attacker, Defender defender, Game game , Maze m) {
-            if(PakuOnPowerPill(attacker,m)){
-                return run(defender,m,attacker,game,0);
+        private int trapperUpdate(Attacker attacker, Defender defender, Game game , Maze m) {
+            Node target = targetInFrontOf(attacker);
+            if (!isPakuOnPath(attacker, defender)) {
+
             }
-            Node target = attacker.getLocation().getNeighbor(attacker.getDirection());
-
-            if (target == null)
-                defender.getNextDir(attacker.getLocation(), !defender.isVulnerable());
-
             else {
-                Node superTarget = target.getNeighbor(attacker.getDirection());
+                if (!defender.getLocation().isJunction()) {
 
-                if (!(superTarget == null) && defender.getLocation().getPathDistance(superTarget) > 3) {
-                    return defender.getNextDir(superTarget, !defender.isVulnerable());
-                } else return defender.getNextDir(attacker.getLocation(), !defender.isVulnerable());
+
+                } else {
+
+
+                }
+
             }
-            return -1;
+            return defender.getNextDir(target, !defender.isVulnerable());
         }
+
+
+        private boolean isPakuOnPath(Attacker attacker, Defender defender) {
+            Node target = targetInFrontOf(attacker);
+            List<Node> listToPaku = defender.getPathTo(target);
+
+            for (Node node: listToPaku) {
+                if (attacker.getLocation().equals(node) ) return true;
+            }
+            return false;
+        }
+
+        private Node targetInFrontOf(Attacker attacker) {
+            Node target = attacker.getLocation().getNeighbor(attacker.getDirection());
+            if (target == null)
+                return attacker.getLocation();
+            return target;
+            }
 
         private boolean PakuOnPowerPill(Attacker Paku,Maze m)
         {
@@ -137,8 +154,6 @@ import java.util.List;
             }
             System.out.println();
         }
-
-
 
         /**
          * Blanchard's default code
